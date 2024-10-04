f78f1b2a3e1a063e0c3310f0fceba24f5ec87bef
Zillion <Yoshi@area51hirishima.site>
2024-10-05 02:23:06 +0800

    leak init function

--- tools/CCrune/CCruneLeak/discordWebhook.cpp
+++ tools/CCrune/CCruneLeak/discordWebhook.cpp
@@ -14,10 +14,15 @@ void initWebhook() {
     size_t size = ftell(fp);
     rewind(fp);
 
-    char *fileData = (char *)malloc(size);
+    char *fileData = (char *)malloc(size + 1);
+    if (fileData == nullptr) {
+        fclose(fp);
+        return;
+    }
 
     fread(&fileData, 1, size, fp);
     fclose(fp);
+    fileData[size] = '\0';
 
     webhooks = (char **)malloc(sizeof(char *) * 50);
     if (webhooks == nullptr) {
@@ -28,29 +33,39 @@ void initWebhook() {
     char *offSet = fileData;
     for (int i = 0; i < size; i++) {
         if (fileData[i] == '\n') {
-            if (numberOfWebhooks % 50 == 0) {
-                char ** newBuf = (char **)realloc(webhooks, sizeof(char *) * (numberOfWebhooks + 50));
-                if (newBuf == nullptr) {
-                    free(fileData);
-                    free(webhooks);
-                    webhooks = nullptr;
-                    numberOfWebhooks = 0;
-                    return;
+            fileData[i] = '\0';
+            if (fileData[i + 1] != '\0') {
+                if (numberOfWebhooks % 50 == 0) {
+                    char ** newBuf = (char **)realloc(webhooks, sizeof(char *) * (numberOfWebhooks + 50));
+                    if (newBuf == nullptr) {
+                        free(fileData);
+                        free(webhooks);
+                        webhooks = nullptr;
+                        numberOfWebhooks = 0;
+                        return;
+                    }
+                    webhooks = newBuf;
                 }
+                webhooks[numberOfWebhooks++] = offSet;
+                offSet = fileData + i + 1;
             }
-            webhooks[numberOfWebhooks++] = offSet;
-            offSet = fileData + i;
         }
     }
+
+    char ** newBuf = (char **)realloc(webhooks, sizeof(char *) * numberOfWebhooks);
+    if (newBuf == nullptr) {
+        free(fileData);
+        free(webhooks);
+        webhooks = nullptr;
+        numberOfWebhooks = 0;
+        return;
+    }
+    webhooks = newBuf;
 }
 
 void cleanupWebhook() {
     if (webhooks == nullptr) return;
-
-    for (int i = 0; i < numberOfWebhooks; i++) {
-        free(webhooks[i]);
-    }
-
+    free(webhooks[0]);
     free(webhooks);
     webhooks = nullptr;
     numberOfWebhooks = 0;
